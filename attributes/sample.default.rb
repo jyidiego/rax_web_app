4c6ef191ef03ff21393ea129a7ba407e26c79b69
John Yi <john.yi@rackspace.com>
2013-10-30 23:54:12 -0400

    add a sample attribute file

---
+++ attributes/sample.default.rb
@@ -0,0 +1,7 @@
+include_attribute "raxdns"
+
+default[:rsdns][:rackspace_username] = 'user'
+default[:rsdns][:rackspace_api_key] = 'b15839ffffff0000000111111f'
+default[:rsdns][:rackspace_auth_region] = 'us'
+default[:domain] = 'rax.net'
+
